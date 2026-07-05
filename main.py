be8ec8d0124c6a7135f46b10c915c9d45f6f2855
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-05 10:47:39 +0300

    finish the part 1 step 3 - Full name

--- main.py
+++ main.py
@@ -3,4 +3,5 @@
 manager_son_price = lambda price, is_manager_son: price * 0.8 if is_manager_son == True else price + (price * 0.17)
 # step 2 - Final price after discount
 final_price = lambda price, discount: price / 100 * (100 - discount)
-print(final_price(200, 10))
+# step 3 - Full name
+full_name = lambda first_name, last_name: f"{first_name} {last_name}" 
