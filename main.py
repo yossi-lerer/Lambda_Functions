d0032e29c66b52083c43a3f380651d2a9a2a9bae
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-05 10:45:11 +0300

    finish the part 1 step 2 - Final price after discount

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 # part 1
 # step 1 - Manager son price
 manager_son_price = lambda price, is_manager_son: price * 0.8 if is_manager_son == True else price + (price * 0.17)
+# step 2 - Final price after discount
+final_price = lambda price, discount: price / 100 * (100 - discount)
+print(final_price(200, 10))
